eed6464235209c6581b85d4d88865542d219c667
otofu-square <yukita@feedforce.jp>
2017-07-17 23:53:02 +0900

    :tada: Hello World from plug

--- elixir/lib/line_webhook.ex
+++ elixir/lib/line_webhook.ex
@@ -1,5 +1,13 @@
 defmodule LineWebhook do
-  def hello do
-    :world
+  import Plug.Conn
+
+  def init(options) do
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello world")
   end
 end
